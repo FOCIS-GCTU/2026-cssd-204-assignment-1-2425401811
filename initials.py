5459c0562532119a8f58b76b991077cf653cef89
2425401811 <2425401811@live.gctu.edu.gh>
2026-05-31 00:57:36 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: A print out of my initials.
+# Assignment Number: 1
+#
+# Name: Mantey Eugene Kwame
+# STUDENT ID:  2425401811
+# Email: 2425401811@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,MANTEY EUGENE KWAME, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #This will create a blank line before printing my initials in capital letters.
+    print()
 
+    print("...MEK")     #This is the first print showing my initials in Uppercase letters.
+    print()     #This will create a blank line between my initials in capitals and my initials in block letters.
+
+    #This will print my initials in block letters(MEK) and periods.
+    print("...MMMM....MMMM........EEEEEEEEEEEE........KK........KK.....")
+    print("...MMMM....MMMM........EE..................KK.......KK......")
+    print("...MM.MM..MM.MM........EE..................KK......KK.......")
+    print("...MM.MM..MM.MM........EE..................KK.....KK........")
+    print("...MM.MM..MM.MM........EEEEEEEEEEEE........KK....KK.........")
+    print("...MM..MMMM..MM........EEEEEEEEEEEE........KK...KK..........")
+    print("...MM..MMMM..MM........EE..................KK.KK..KK........")
+    print("...MM...MM...MM........EE..................KK......KK.......")
+    print("...MM...MM...MM...**...EE.............**...KK.......KK....**")
+    print("...MM...MM...MM...**...EEEEEEEEEEEE...**...KK........KK...**")
+
+    print()      #This is the last blank line after printing my initials in block letters.
 
 
 main()
